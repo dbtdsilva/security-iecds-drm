1681e429776fca4343119c760334dafacc52494c
Diogo Silva <dbtds@ua.pt>
2015-11-15 16:19:34 +0000

    Updated install and visual

--- install.sh
+++ install.sh
@@ -1,6 +1,11 @@
 #/bin/bash
 sudo apt-get install postgresql postgresql-contrib postgresql-client pgadmin3 postgresql-server-dev-9.4 \
-	python-dev libgeoip-dev libffi-dev npm
+	python-dev libgeoip-dev libffi-dev npm curl
+curl ftp://ftp.videolan.org/pub/debian/videolan-apt.asc | sudo apt-key add -
+echo "deb ftp://ftp.videolan.org/pub/debian/stable ./" | sudo tee /etc/apt/sources.list.d/libdvdcss.list
+sudo apt-get update
+sudo apt-get install vlc vlc-data vlc-plugin-pulse browser-plugin-vlc mplayer2 
+sudo apt-get install ubuntu-restricted-extras
 sudo -u postgres createdb security
 sudo -u postgres psql security -c "\password"
 # 7yl74Zm4ZpcEsPMilEqUa4vNuRt7jvzm
