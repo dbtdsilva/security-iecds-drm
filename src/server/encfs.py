5d2179d128f1d69b677829ab33bebaa7381f1a4f
Diogo Silva <dbtds@ua.pt>
2015-12-28 22:02:44 +0000

    Updated scripts, only mounts when file is needed or its being used

--- src/server/encfs.py
+++ src/server/encfs.py
@@ -2,43 +2,47 @@ import subprocess
 import shutil
 import os
 import binascii
+import threading
 
 media_dir = "files"
 mount_dir = "mount"
 store_dir = "store"
 
-def insert_encrypted_file(filename, encfs_password):
-    title = binascii.hexlify(filename)
+class EncryptedFileSystemMedia:
+    def __init__(self):
+        self.requests = {}
+        self.access = threading.Lock()
 
-    mount_f = media_dir + os.path.sep + mount_dir + os.path.sep + title
-    store_f = media_dir + os.path.sep + store_dir + os.path.sep + title
+    def mount_encrypted_file(self, filename, encfs_password):
+        self.access.acquire()
+        try:
+            title = binascii.hexlify(filename)
+            mount_f = media_dir + os.path.sep + mount_dir + os.path.sep + title
+            store_f = media_dir + os.path.sep + store_dir + os.path.sep + title
 
-    if not os.path.exists(mount_f):
-        os.makedirs(mount_f)
-    if not os.path.exists(store_f):
-        os.makedirs(store_f)
+            if not os.path.exists(mount_f + os.path.sep + filename):
+                subprocess.call(["/bin/sh", "-c", 'echo '+encfs_password+' | encfs -S '+
+                                 os.path.abspath(store_f)+' '+os.path.abspath(mount_f)])
+            f = open(mount_f + os.path.sep + filename)
+            return f
+        except Exception:
+            return None
+        finally:
+            if filename not in self.requests:
+                self.requests[filename] = 0
+            else:
+                self.requests[filename] += 1
+            self.access.release()
 
-    subprocess.call(["expect", "encfs.exp", encfs_password, os.path.abspath(mount_f), os.path.abspath(store_f)])
-    subprocess.call(["/bin/sh", "-c", 'echo '+encfs_password+' | encfs -S '+os.path.abspath(store_f)+' '+os.path.abspath(mount_f)])
-    shutil.copyfile(filename, mount_f + os.path.sep + filename)
-    subprocess.call(["fusermount", "-u", os.path.abspath(mount_f)])
+    def unmount_encrypted_file(self, filename):
+        self.access.acquire()
 
-def mount_encrypted_file(filename, encfs_password):
-    title = binascii.hexlify(filename)
-    mount_f = media_dir + os.path.sep + mount_dir + os.path.sep + title
-    store_f = media_dir + os.path.sep + store_dir + os.path.sep + title
-    if not os.path.exists(mount_f + os.path.sep + filename):
-        subprocess.call(["/bin/sh", "-c", 'echo '+encfs_password+' | encfs -S '+
-                         os.path.abspath(store_f)+' '+os.path.abspath(mount_f)])
-    return open(mount_f + os.path.sep + filename)
-
-def unmount_encrypted_file(filename):
-    title = binascii.hexlify(filename)
-    mount_f = media_dir + os.path.sep + mount_dir + os.path.sep + title
-    subprocess.call(["fusermount", "-u", os.path.abspath(mount_f)])
-
-#insert_encrypted_file('sample.mkv', 'abc')
-#f = mount_encrypted_file('sample.mkv', 'abc')
-#print f.read()
-#f.close()
-#unmount_encrypted_file('sample.mkv')
+        try:
+            title = binascii.hexlify(filename)
+            mount_f = media_dir + os.path.sep + mount_dir + os.path.sep + title
+            if filename in self.requests:
+                self.requests[filename] -= 1
+                if self.requests[filename] == 0:
+                    subprocess.call(["fusermount", "-u", os.path.abspath(mount_f)])
+        finally:
+            self.access.release()
